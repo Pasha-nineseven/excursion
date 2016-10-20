9b4d0727b979d5f7da125b52e6bc38d8151a131f
Pasha-nineseven <pavel@nineseven.ru>
2016-10-20 12:43:37 +0300

    favicon

--- dist/js/interface.js
+++ dist/js/interface.js
@@ -259,36 +259,38 @@ $(window).resize(function () {
 });
 
 $(window).load(function(){
-	//Check File API support
-    if(window.File && window.FileList && window.FileReader)
-    {
-        var filesInput = document.getElementById("files");
-        filesInput.addEventListener("change", function(event){
-            var files = event.target.files;
-            var output = document.getElementById("result");
-            for(var i = 0; i< files.length; i++)
-            {
-                var file = files[i];
-                if(!file.type.match('image'))
-                  continue;
-                var picReader = new FileReader();
-                picReader.addEventListener("load",function(event){
-                    var picFile = event.target;
-                    var div = document.createElement("div");
-                    div.className = 'photo-list__item';
-                    div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
-                            "title='" + picFile.name + "'/><a href='#' class='del'></a>";
-                    output.insertBefore(div,null);            
-                });
-                picReader.readAsDataURL(file);
-            }                               
-           
-        });
-    }
-    else
-    {
-        console.log("Your browser does not support File API");
-    }
+	if ($('#files').length>0) {
+		//Check File API support
+	    if(window.File && window.FileList && window.FileReader)
+	    {
+	        var filesInput = document.getElementById("files");
+	        filesInput.addEventListener("change", function(event){
+	            var files = event.target.files;
+	            var output = document.getElementById("result");
+	            for(var i = 0; i< files.length; i++)
+	            {
+	                var file = files[i];
+	                if(!file.type.match('image'))
+	                  continue;
+	                var picReader = new FileReader();
+	                picReader.addEventListener("load",function(event){
+	                    var picFile = event.target;
+	                    var div = document.createElement("div");
+	                    div.className = 'photo-list__item';
+	                    div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
+	                            "title='" + picFile.name + "'/><a href='#' class='del'></a>";
+	                    output.insertBefore(div,null);            
+	                });
+	                picReader.readAsDataURL(file);
+	            }                               
+	           
+	        });
+	    }
+	    else
+	    {
+	        console.log("Your browser does not support File API");
+	    }
+    };
 });
 
 // functions
